a2f6d4850da36a70792f76bccdabd4f2e6da60af
Lucas Diaz Miguez <lucadiazmiguez@gmail.com>
2021-04-12 23:24:05 -0300

    working on the react application

---
+++ my-app2/src/components/ItemListContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export const ItemListContainer= (props)=>{
+    return (
+        <h2> welcome to React Js</h2>
+    )
+}
---
+++ my-app2/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+export function NavBar(props) {
+    return (
+        <nav>
+                <h1> Brand </h1>
+                <input type="text" placeholder="buscar"/>
+                <i aria-hidden="true"></i>
+                <a href="">inicio </a>
+                <a href="">Iniciar Sesión</a>
+                <a href="">Crear Usuario</a>
+                <a href="">Carrito 0</a>
+        </nav>) 
+        };
